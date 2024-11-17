63a0ac15709172a1a293a970e4632209c4ce98e2
twy8939 <twy8939@naver.com>
2024-11-17 15:47:22 +0900

    feat : Add Model Select Action

--- components/chat/ModelSelect.tsx
+++ components/chat/ModelSelect.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Select,
@@ -6,18 +8,28 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
+import { useModelStore } from "@/store/model";
 
 const MODELS = ["gpt-3.5-turbo", "gpt-4", "gpt-4-4o"];
 
 export default function ModelSelect() {
+  const currentModel = useModelStore((state) => state.model);
+  const updateModel = useModelStore((state) => state.updateModel);
+  const handleChange = (selectModel: string) => {
+    updateModel(selectModel);
+  };
   return (
-    <Select>
+    <Select value={currentModel} onValueChange={handleChange}>
       <SelectTrigger className="w-[180px] border-none text-xl focus:ring-transparent">
         <SelectValue placeholder="모델 선택" />
       </SelectTrigger>
       <SelectContent>
         {MODELS.map((model) => (
-          <SelectItem key={model} value={model}>
+          <SelectItem
+            key={model}
+            value={model}
+            disabled={currentModel === model}
+          >
             {model}
           </SelectItem>
         ))}
