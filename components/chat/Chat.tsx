d26b6d05ef40101bf511599298c253ae6ccc8589
twy8939 <twy8939@naver.com>
2024-11-22 22:54:24 +0900

    feat : integrate OpenAI API via Vercel AI SDK

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,35 +1,34 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Empty from "./Empty";
 import Message from "./Message";
 import AutoResizingTextarea from "./AutoResizingTextarea";
 import { Button } from "../ui/button";
 import { ArrowUp } from "lucide-react";
-
-const MESSAGE_DUMMY = [
-  { id: 1, content: "hi", role: "user" },
-  { id: 2, content: "hello", role: "assistant" },
-];
+import { useChat } from "ai/react";
+import { useModelStore } from "@/store/model";
 
 export default function Chat() {
-  const [value, setValue] = useState("");
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const model = useModelStore((store) => store.model);
   const scrollRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (scrollRef.current) {
       scrollRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  });
+  }, [messages]);
+
   return (
     <div className="flex flex-col w-[80%] h-full mx-auto">
       {/* 채팅 영역 */}
       <div className="flex-1">
-        {MESSAGE_DUMMY.length === 0 ? (
+        {messages.length === 0 ? (
           <Empty />
         ) : (
           <>
-            {MESSAGE_DUMMY.map((message) => (
+            {messages.map((message) => (
               <Message key={message.id} name="user" {...message} />
             ))}
           </>
@@ -37,11 +36,11 @@ export default function Chat() {
       </div>
       {/* input 영역 */}
       <div className="sticky bottom-0 pb-5 bg-white">
-        <form className="flex items-center justify-center gap-4">
-          <AutoResizingTextarea
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
+        <form
+          className="flex items-center justify-center gap-4"
+          onSubmit={(e) => handleSubmit(e, { data: model })}
+        >
+          <AutoResizingTextarea value={input} onChange={handleInputChange} />
           <Button type="submit" size={"icon"}>
             <ArrowUp />
           </Button>
